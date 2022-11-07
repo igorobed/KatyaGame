811be1f1b89ab934360777c1e75551be757345e5
DungeonMaster3000 <igorobed@yandex.ru>
2022-11-07 04:19:16 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def test_func() -> None:
+    """
+
+    :return:
+    """
+    print("Что-то...")
+
+
+if __name__ == "__main__":
+    my_str: str = str(4)
+    print(my_str)  # hhhhhhhhhhhhhhhhhhhhhhh
